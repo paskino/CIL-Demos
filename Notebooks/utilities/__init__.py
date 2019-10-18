65920f7bf48b0e46b0fb6a5518dc060c59b02673
rwarr95 <ryan.warr@postgrad.manchester.ac.uk>
2019-10-18 10:54:03 +0100

    merge init from master

--- Notebooks/utilities/__init__.py
+++ Notebooks/utilities/__init__.py
@@ -3,11 +3,11 @@ from ipywidgets import interact, interactive, fixed, interact_manual
 import ipywidgets as widgets
 import matplotlib.pyplot as plt
 from matplotlib import gridspec
+import numpy
 
-
-def display_slice(datacontainer, direction, title='Title'):
+def display_slice(container, direction, title, cmap, minmax):
     
-    container = datacontainer.as_array()
+        
     def get_slice_3D(x):
         
         if direction == 0:
@@ -16,32 +16,65 @@ def display_slice(datacontainer, direction, title='Title'):
             img = container[:,x,:]
         elif direction == 2:
             img = container[:,:,x]
+        
         fig = plt.figure()
         gs = gridspec.GridSpec(1, 2, figure=fig, width_ratios=(1,.05))
         # image
         ax = fig.add_subplot(gs[0, 0])
-        aximg = ax.imshow(img)
-        ax.set_title("{} {}".format(title, x))
+        aximg = ax.imshow(img, cmap=cmap)
+        aximg.set_clim(minmax)
+        ax.set_title(title + " {}".format(x))
         # colorbar
         ax = fig.add_subplot(gs[0, 1])
         plt.colorbar(aximg, cax=ax)
         plt.tight_layout()
-        plt.show()
+        plt.show(fig)
         
     return get_slice_3D
     
-def islicer(data, direction,title='Title'):
+def islicer(data, direction, title="", cmap='viridis', minmax=None):
     '''Creates an interactive integer slider that slices a 3D volume along direction
     
-    :param data: DataContainer
+    :param data: DataContainer or numpy array
     :param direction: slice direction, int, should be 0,1,2 or the axis label
+    :param title: optional title for the display
     '''
-    if direction in data.dimension_labels.values():
-        direction = data.get_dimension_axis(direction)
-    interact(display_slice(data,direction,title), 
-         x=widgets.IntSlider(min=0, max=data.shape[direction]-1, step=1, 
-                             value=0, continuous_update=False));
     
+    if hasattr(data, "as_array"):
+        container = data.as_array()
+        if not isinstance (direction, int):
+            if direction in data.dimension_labels.values():
+                direction = data.get_dimension_axis(direction)
+    elif isinstance (data, numpy.ndarray):
+        container = data
+        
+    
+    slider = widgets.IntSlider(min=0, max=data.shape[direction]-1, step=1, 
+                             value=0, continuous_update=False)
+
+    if minmax is None:
+        amax = container.max()
+        amin = container.min()
+    else:
+        amin = min(minmax)
+        amax = max(minmax)
+    
+    interact(display_slice(container, 
+                           direction, 
+                           title=title, 
+                           cmap=cmap, 
+                           minmax=(amin, amax)),
+             x=slider);
+    return slider
+    
+
+def link_islicer(*args):
+    '''links islicers IntSlider widgets'''
+    linked = [(widg, 'value') for widg in args]
+    # link pair-wise
+    pairs = [(linked[i+1],linked[i]) for i in range(len(linked)-1)]
+    for pair in pairs:
+        widgets.link(*pair)
     
 
 def setup_iplot2D(x_init):
@@ -60,13 +93,3 @@ def setup_iplot2D(x_init):
     
 
 
-def iplot2D(fig, ax, im, iterations, residuals):
-    '''callback to change the matplotlib figure created with setup_iplot2D'''
-    def update(iteration, last_objective, x):
-        residuals.append(last_objective)
-        iterations.append(iteration)
-        ax.clear()
-        ax.plot(iterations, residuals)
-        im.imshow(x.as_array())
-        fig.canvas.draw()
-    return update

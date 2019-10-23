3717a142a3d5e21175b2bfe1895c38584f197fe8
epapoutsellis <epapoutsellis@gmail.com>
2019-10-23 17:28:57 +0100

    update show

--- Notebooks/utilities/show_utilities.py
+++ Notebooks/utilities/show_utilities.py
@@ -141,46 +141,7 @@ def show(x, title = None, show_channels = [1], **kwargs):
         elif len(x.shape[1:]) == 3:
             show3D_channels(x, title, **kwargs)  
             
-            
-            
-from IPython.display import HTML
-import random
 
-# https://stackoverflow.com/questions/31517194/how-to-hide-one-specific-cell-input-or-output-in-ipython-notebook/52664156
-
-def hide_toggle(for_next=False):
-    this_cell = """$('div.cell.code_cell.rendered.selected')"""
-    next_cell = this_cell + '.next()'
-
-    toggle_text = 'Toggle show/hide'  # text shown on toggle link
-    target_cell = this_cell  # target cell to control with toggle
-    js_hide_current = ''  # bit of JS to permanently hide code in current cell (only when toggling next cell)
-
-    if for_next:
-        target_cell = next_cell
-        toggle_text += ' next cell'
-        js_hide_current = this_cell + '.find("div.input").hide();'
-
-    js_f_name = 'code_toggle_{}'.format(str(random.randint(1,2**64)))
-
-    html = """
-        <script>
-            function {f_name}() {{
-                {cell_selector}.find('div.input').toggle();
-            }}
-
-            {js_hide_current}
-        </script>
-
-        <a href="javascript:{f_name}()">{toggle_text}</a>
-    """.format(
-        f_name=js_f_name,
-        cell_selector=target_cell,
-        js_hide_current=js_hide_current, 
-        toggle_text=toggle_text
-    )
-
-    return HTML(html)            
             
 if __name__ == '__main__':         
     

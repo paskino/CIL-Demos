93ef6e01a6e81b4f3cbb9022c7539c3fbcc68386
rwarr95 <ryan.warr@postgrad.manchester.ac.uk>
2019-10-30 18:21:28 +0000

    Updated to latest version, with labelled islicer

--- Notebooks/utilities/__init__.py
+++ Notebooks/utilities/__init__.py
@@ -6,18 +6,31 @@ import matplotlib.pyplot as plt
 from matplotlib import gridspec
 import numpy
 
-def display_slice(container, direction, title, cmap, minmax, size):
+def display_slice(container, direction, title, cmap, minmax, size, axis_labels):
     
         
     def get_slice_3D(x):
         
         if direction == 0:
             img = container[x]
+            x_lim = container.shape[2]
+            y_lim = container.shape[1]
+            x_label = axis_labels[2]
+            y_label = axis_labels[1] 
+            
         elif direction == 1:
             img = container[:,x,:]
+            x_lim = container.shape[2]
+            y_lim = container.shape[0] 
+            x_label = axis_labels[2]
+            y_label = axis_labels[0]             
+            
         elif direction == 2:
             img = container[:,:,x]
-        
+            x_lim = container.shape[1]
+            y_lim = container.shape[0]    
+            x_label = axis_labels[1]
+            y_label = axis_labels[0]             
         
         if size is None:
             fig = plt.figure()
@@ -32,7 +45,11 @@ def display_slice(container, direction, title, cmap, minmax, size):
         gs = gridspec.GridSpec(1, 2, figure=fig, width_ratios=(1,.05), height_ratios=(1,))
         # image
         ax = fig.add_subplot(gs[0, 0])
-        aximg = ax.imshow(img, cmap=cmap)
+      
+        ax.set_xlabel(x_label)     
+        ax.set_ylabel(y_label)
+ 
+        aximg = ax.imshow(img, cmap=cmap, origin='upper', extent=(0,x_lim,y_lim,0))
         aximg.set_clim(minmax)
         ax.set_title(dtitle + " {}".format(x))
         # colorbar
@@ -44,7 +61,7 @@ def display_slice(container, direction, title, cmap, minmax, size):
     return get_slice_3D
 
     
-def islicer(data, direction, title="", slice_number=None, cmap='inferno', minmax=None, size=None):
+def islicer(data, direction, title="", slice_number=None, cmap='gray', minmax=None, size=None, axis_labels=None):
 
     '''Creates an interactive integer slider that slices a 3D volume along direction
     
@@ -57,18 +74,28 @@ def islicer(data, direction, title="", slice_number=None, cmap='inferno', minmax
     :param size: int or tuple specifying the figure size in inch. If int it specifies the width and scales the height keeping the standard matplotlib aspect ratio 
     '''
     
+    if axis_labels is None:
+        if hasattr(data, "dimension_labels"):
+            axis_labels = [data.dimension_labels[0],data.dimension_labels[1],data.dimension_labels[2]]
+        else:
+            axis_labels = ['X', 'Y', 'Z']
+
+    
     if hasattr(data, "as_array"):
         container = data.as_array()
+        
         if not isinstance (direction, int):
             if direction in data.dimension_labels.values():
-                direction = data.get_dimension_axis(direction)
+                direction = data.get_dimension_axis(direction)                             
+
     elif isinstance (data, numpy.ndarray):
         container = data
         
     if slice_number is None:
         slice_number = int(data.shape[direction]/2)
+        
     slider = widgets.IntSlider(min=0, max=data.shape[direction]-1, step=1, 
-                             value=slice_number, continuous_update=False)
+                             value=slice_number, continuous_update=False, description=axis_labels[direction])
 
     if minmax is None:
         amax = container.max()
@@ -86,8 +113,9 @@ def islicer(data, direction, title="", slice_number=None, cmap='inferno', minmax
                            title=title, 
                            cmap=cmap, 
                            minmax=(amin, amax),
-                           size=size),
+                           size=size, axis_labels=axis_labels),
              x=slider);
+    
     return slider
     
 
@@ -106,8 +134,8 @@ def psnr(img1, img2, data_range=1):
     return 20 * numpy.log10(data_range / numpy.sqrt(mse))
 
 
-def plotter2D(datacontainers, titles, fix_range=False, stretch_y=False, cmap='gnuplot'):
-    '''plotter2D(datacontainers, titles, fix_range=False, stretch_y=False)
+def plotter2D(datacontainers, titles=None, fix_range=False, stretch_y=False, cmap='gray', axis_labels=None):
+    '''plotter2D(datacontainers=[], titles=[], fix_range=False, stretch_y=False, cmap='gray', axes_labels=['X','Y'])
     
     plots 1 or more 2D plots in an (n x 2) matix
     multiple datasets can be passed as a list
@@ -117,8 +145,11 @@ def plotter2D(datacontainers, titles, fix_range=False, stretch_y=False, cmap='gn
     if(isinstance(datacontainers, list)) is False:
         datacontainers = [datacontainers]
 
-    if(isinstance(titles, list)) is False:
-        titles = [titles]
+    if titles is not None:
+        if(isinstance(titles, list)) is False:
+            titles = [titles]
+            
+
     
     nplots = len(datacontainers)
     rows = int(round((nplots+0.5)/2.0))
@@ -145,14 +176,26 @@ def plotter2D(datacontainers, titles, fix_range=False, stretch_y=False, cmap='gn
 
     for i in range(nplots):
         axes[i].set_visible(True)
-        axes[i].set_title(titles[i])
-       
-        if type(datacontainers[i]) is numpy.ndarray:
-            dc = datacontainers[i]
-        else:
-            dc = datacontainers[i].as_array()    
         
-        sp = axes[i].imshow(dc, cmap=cmap)
+        if titles is not None:
+            axes[i].set_title(titles[i])
+       
+        if axis_labels is not None:
+            axes[i].set_ylabel(axis_labels[1])
+            axes[i].set_xlabel(axis_labels[0]) 
+            
+        if type(datacontainers[i]) is numpy.ndarray:
+            dc = datacontainers[i]          
+        else:
+            dc = datacontainers[i].as_array()
+            
+            if axis_labels is None:
+                axes[i].set_ylabel(datacontainers[i].dimension_labels[0])
+                axes[i].set_xlabel(datacontainers[i].dimension_labels[1])        
+        
+        
+        sp = axes[i].imshow(dc, cmap=cmap, origin='upper', extent=(0,dc.shape[1],dc.shape[0],0))
+    
         
         im_ratio = dc.shape[0]/dc.shape[1]
         

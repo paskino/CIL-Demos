a1f9bbb8e019b40c72ac4917602b52dd2982981a
Edoardo Pasca <edo.paskino@gmail.com>
2019-12-06 17:27:54 +0000

    Fix 19.10 (#36)
    * updated sino_ideal.nxs for Python 3
    
    * updated nexus_ideal.nxs
    
    * updated flats and darks
    
    * updated proj.nxs

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -53,33 +53,36 @@ rm -f ${filename1}.md5 # (re)create md5 checksum
 echo "fccb00c33969c3a3ee6dbb915cdc923e ${filename1}" > ${filename1}.md5
 download ${URL} ${filename1} ${suffix}
 
-URL=https://www.dropbox.com/s/08g3db2o97mnr4i/
+URL=https://www.ccpi.ac.uk/sites/www.ccpi.ac.uk/files/
 filename2=dark.nxs
 suffix=?dl=0
 rm -f ${filename2}.md5 # (re)create md5 checksum
-echo "8de121d4e32347611cf860c38dcafb62 ${filename2}" > ${filename2}.md5
-download ${URL} ${filename2} ${suffix}
+echo "d5bab6a508e7c5f36dd9803d89b9655b ${filename2}" > ${filename2}.md5
+download ${URL} ${filename2}
 
-URL=https://www.dropbox.com/s/x42skaybevmwwsv/
+URL=https://www.ccpi.ac.uk/sites/www.ccpi.ac.uk/files/
 filename3=flat.nxs
-suffix=?dl=0
 rm -f ${filename3}.md5 # (re)create md5 checksum
-echo "714fbe1f80ac3a6b84c1bd2a9a274787 ${filename3}" > ${filename3}.md5
-download ${URL} ${filename3} ${suffix}
+echo "cf6d244b15140479495801031115ed90 ${filename3}" > ${filename3}.md5
+download ${URL} ${filename3}
 
-URL=https://www.dropbox.com/s/8ajcss2aipoupfw/
 filename4=proj.nxs
-suffix=?dl=0
 rm -f ${filename4}.md5 # (re)create md5 checksum
-echo "f7043889384e6f49887d0a84ad7af400 ${filename4}" > ${filename4}.md5
+echo "9f8688e8e77c7677ef47f39b655a6ae8 ${filename4}" > ${filename4}.md5
 download ${URL} ${filename4} ${suffix}
 
-URL=https://www.dropbox.com/s/g04vy3k4ao92zs2/
+#URL=https://www.dropbox.com/s/g04vy3k4ao92zs2/
+#filename5=sino_ideal.nxs
+#suffix=?dl=0
+#rm -f ${filename5}.md5 # (re)create md5 checksum
+#echo "75a60f202b6c06457634cc45f598271e ${filename5}" > ${filename5}.md5
+#download ${URL} ${filename5} ${suffix}
+
+URL=https://www.ccpi.ac.uk/sites/www.ccpi.ac.uk/files/
 filename5=sino_ideal.nxs
-suffix=?dl=0
 rm -f ${filename5}.md5 # (re)create md5 checksum
-echo "75a60f202b6c06457634cc45f598271e ${filename5}" > ${filename5}.md5
-download ${URL} ${filename5} ${suffix}
+echo "3c5851d81a67123d3a0a508d3e05158a ${filename5}" > ${filename5}.md5
+download ${URL} ${filename5}
 
 # Get Zenodo dataset
 # URL=https://zenodo.org/record/2633785/files/
